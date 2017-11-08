612a67bf2742a674902699ed35d8746078517d53
Bechkalo Roman <bechkalor@gmail.com>
2017-11-08 13:28:15 +0000

    Changet

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const a = Number(prompt('A= '));
-const b = Number(prompt('B= '));
-const c = Number(prompt('C= '));
-document.write('A='+ a +'<br>');
+const yourName = prompt('Your name?  ');
+document.write('Your name:'+'   ' + yourName + '<br>');
+alert('Calculate the square aquation: Ax^2+Bx+C=0  ?');
+
+const a = Number(prompt('Enter the value:\n' + 'A= '));
+const b = Number(prompt('Enter the value:\n' + 'B= '));
+const c = Number(prompt('Enter the value:\n' + 'C= '));
+/*document.write('A='+ a +'<br>');
 document.write('B='+ b +'<br>');
-document.write('C='+ c +'<br>');
+document.write('C='+ c +'<br>');*/
+document.write('Your equation:');
+document.write('<u>'+ a + 'x^2+ ' + b + 'x+ ' + c + ' =0'+'</u>');
 
 function sqrt (a,b,c){
          let x,x1,x2;
@@ -12,7 +18,7 @@ function sqrt (a,b,c){
         if(d>0){
                 x1 = (-b+Math.sqrt(d))/(2*a);
                 x2 = (-b-Math.sqrt(d))/(2*a);
-                return 'X1= '+ x1+',X2= '+x2;
+                return  '<br>' +'X1= '+ x1 + '<br>' +'X2= '+x2;
             }
         else if (d==0){
             x = -b/(2*a);
@@ -22,8 +28,12 @@ function sqrt (a,b,c){
             return 'd<0. Not result';
             }
 }
+
 function disc(a,b,c){
 return  (b*b)-(4*a*c);
 }
+
+document.write('<br>' + 'Result:  ');
 const result = sqrt(a,b,c);
 document.write(result);
+
