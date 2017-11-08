91a5cef2f7146eeb7aecbab120e59f34d651cd90
Bechkalo Roman <bechkalor@gmail.com>
2017-11-08 14:55:58 +0000

    Initial two

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
-const yourName = prompt('Hello! Your name?  ');
-document.write('Hello'+'   ' + yourName + '!'+ '<br>');
-alert('Calculate the square aquation: Ax^2+Bx+C=0  ?');
-
 const a = Number(prompt('Enter the value:\n' + 'A= '));
 const b = Number(prompt('Enter the value:\n' + 'B= '));
 const c = Number(prompt('Enter the value:\n' + 'C= '));
 document.write('Your equation:');
-document.write('<u>'+ a + 'x^2+ ' + b + 'x+ ' + c + ' =0'+'</u>');
+document.write('<u>' + a + 'x^2+ ' + b + 'x+ ' + c + ' =0' + '</u>');
 
-function sqrt (a,b,c){
-         let x,x1,x2;
-        // const d = (b*b)-(4*a*c);
-        const d = disc(a,b,c);
-        if(d>0){
-                x1 = (-b+Math.sqrt(d))/(2*a);
-                x2 = (-b-Math.sqrt(d))/(2*a);
-                return  '<br>' +'X1= '+ x1 + '<br>' +'X2= '+x2;
-            }
-        else if (d===0){
-            x = -b/(2*a);
-            return 'Discriminator = 0. So one answer: X= ' + x;
-            } 
-            return 'd<0. Not result';
+function quadraticEquation(a, b, c) {
+    let x, x1, x2;
+    const d = disc(a, b, c);
+    if (d > 0) {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        return '<br>' + 'X1= ' + x1 + '<br>' + 'X2= ' + x2;
+    }
+    else if (d === 0) {
+        x = -b / (2 * a);
+        return 'Discriminator = 0. So one answer: X= ' + x;
+    }
+    return 'd<0. Not result';
 }
 
-function disc(a,b,c){
-return  (b*b)-(4*a*c);
+function disc(a, b, c) {
+    return (b * b) - (4 * a * c);
 }
 
 document.write('<br>' + 'Your result:  ');
-const result = sqrt(a,b,c);
+const result = quadraticEquation(a, b, c);
 document.write(result);
-
